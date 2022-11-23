acee452c9cd6d19146868a08c23b30efd873835f
nathan12212 <tammagno@gmail.com>
2022-11-23 01:07:23 +0000

    Primeiro Commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("A testar meu Java Script");
+
+let firstName="Nathan";
+console.log(firstName);
+console.log(typeof firstName);
+let caneta = {
+    penName: "Caneta Azul",
+    penColor: "Blue",
+    penAvaliable: false
+}
+
+console.log(caneta);
